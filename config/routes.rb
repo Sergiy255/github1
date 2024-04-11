a4b506f21cf40205203910a77b2faf710ca31438
Sergiy255 <simasf@i.ua>
2024-04-11 10:07:48 +0200

    good

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
+
+get 'static_pages/digitalboomru'
+
   get 'static_pages/home'
   get 'static_pages/help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
